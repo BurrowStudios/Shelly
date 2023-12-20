f2d0fe1327a95a47df8f244613371b77d88deeb0
TurtleException <turtle.exception@eldritch.de>
2023-12-20 03:37:28 +0100

    Make DatabaseException unchecked

--- src/main/java/org/burrow_studios/shelly/database/DatabaseException.java
+++ src/main/java/org/burrow_studios/shelly/database/DatabaseException.java
@@ -1,6 +1,6 @@
 package org.burrow_studios.shelly.database;
 
-public class DatabaseException extends Exception {
+public class DatabaseException extends RuntimeException {
     public DatabaseException(Throwable cause) {
         super(cause);
     }
