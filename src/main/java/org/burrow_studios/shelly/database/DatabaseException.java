30319f8c86c3515509eb2ef315011d97cffdc710
TurtleException <turtle.exception@eldritch.de>
2024-02-12 04:05:05 +0100

    Clear repository

--- src/main/java/org/burrow_studios/shelly/database/DatabaseException.java
+++
@@ -1,7 +0,0 @@
-package org.burrow_studios.shelly.database;
-
-public class DatabaseException extends RuntimeException {
-    public DatabaseException(Throwable cause) {
-        super(cause);
-    }
-}
